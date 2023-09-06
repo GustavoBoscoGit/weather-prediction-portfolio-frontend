59449e8038b69ee8e2bed8f4bf88d1ba9f58eb26
GustavoBoscoGit <gustavobosco1@gmail.com>
2023-09-06 20:19:32 -0300

    alteração no front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conteiner">
+      <div className="cidade">
+        <h1>Florianópolis</h1>
+      </div>
     </div>
   );
 }
