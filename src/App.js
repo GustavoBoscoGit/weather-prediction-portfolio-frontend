43b7b4ab8ba6752aaf637b286b9ad63af5262063
GustavoBoscoGit <gustavobosco1@gmail.com>
2023-09-09 21:10:05 -0300

    cards da previsão da semana

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { WiRainMix, WiThermometer } from "react-icons/wi";
 import "./index.css";
 
+
 function App() {
   const [currentDate, setCurrentDate] = useState(new Date());
 
@@ -21,6 +22,15 @@ function App() {
     locale: 'pt-BR'
   };
 
+  const cardDate = {
+    day: '2-digit',
+    month: 'short',
+    timeZone: 'UTC',
+    locale: 'pt-BR'
+  };
+
+  const formatedCardDate = currentDate.toLocaleDateString(undefined, cardDate).replace(' de', '').replace('.', '');
+
   const formattedDate = currentDate.toLocaleDateString(undefined, options);
 
   return (
@@ -36,11 +46,37 @@ function App() {
         </div>
       </div>
       <div className="conteiner">
-        <p className='climaDia'>Chuvoso <WiRainMix size={30}/></p>
+        <p className='climaDia'>Chuvoso <WiRainMix size={30} /></p>
       </div>
       <div className="conteiner">
         <p className='temperaturaDia'>42</p>
-        <div><WiThermometer className="WiThermometer" size={50}/></div>
+        <div><WiThermometer className="WiThermometer" size={50} /></div>
+      </div>
+      <div className="conteiner">
+        <div className='cardsPrevisaoSemana'>
+          <div className='cardtestConteiner'>
+            <div>
+              <p className='temperaturaDiaCard'>20</p>
+              <p className='iconeClimaDiaCard'><WiRainMix size={40} style={{ background: 'transparent' }} /></p>
+              <p className='dataMesCard'>{formatedCardDate}</p>
+            </div>
+            <div>
+              <p className='temperaturaDiaCard'>20</p>
+              <p className='iconeClimaDiaCard'><WiRainMix size={40} style={{ background: 'transparent' }} /></p>
+              <p className='dataMesCard'>{formatedCardDate}</p>
+            </div>
+            <div>
+              <p className='temperaturaDiaCard'>20</p>
+              <p className='iconeClimaDiaCard'><WiRainMix size={40} style={{ background: 'transparent' }} /></p>
+              <p className='dataMesCard'>{formatedCardDate}</p>
+            </div>
+            <div>
+              <p className='temperaturaDiaCard'>20</p>
+              <p className='iconeClimaDiaCard'><WiRainMix size={40} style={{ background: 'transparent' }} /></p>
+              <p className='dataMesCard'>{formatedCardDate}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
