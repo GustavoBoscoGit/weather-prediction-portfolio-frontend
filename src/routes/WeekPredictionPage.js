509073abdf3fae9e4815dd1c05918c2566ef18f9
GustavoBoscoGit <gustavobosco1@gmail.com>
2023-10-20 20:29:36 -0300

    card ani imation

--- src/routes/WeekPredictionPage.js
+++ src/routes/WeekPredictionPage.js
@@ -6,6 +6,10 @@ import { motion } from 'framer-motion';
 import { useState } from "react";
 
 function WeekPredictionPage() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return(
         <div>
             <div className="conteiner">
@@ -23,15 +27,32 @@ function WeekPredictionPage() {
             <div className="conteiner">
                 <div className="cardsDaSemana">
                     <div className="cardsSemanaConteiner">
-                        <motion.div className="card1">
-                            <motion.h2 className="card1Title">Framer Animation Testing</motion.h2>
-                            <motion.div className="card1Body">
-                                <p>Test</p>
-                            </motion.div>
+                        <motion.div transition={{ layout: { duration:1, type: "spring" }}} layout onClick={()=> setIsOpen(!isOpen)} className="card1">
+                            <motion.h2 layout="position" className="card1Title">Framer Motion</motion.h2>
+                            {isOpen &&(
+                                <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1}} className="card1Body">
+                                    <p>
+                                        test test test test test test test test test test test test test test
+                                    </p>
+                                    <p>
+                                        test test test test test test test test test 
+                                    </p>
+                                </motion.div>
+                            )}
                         </motion.div>
-                        <motion.div></motion.div>
-                        <motion.div></motion.div>
-                        <motion.div></motion.div>
+                        <motion.div transition={{ layout: { duration:1, type: "spring" }}} layout onClick={()=> setIsOpen(!isOpen)} className="card2">
+                            <motion.h2 layout="position" className="card2Title">Framer Motion</motion.h2>
+                            {isOpen &&(
+                                <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1}} className="card2Body">
+                                    <p>
+                                        test test test test test test test test test test test test test test
+                                    </p>
+                                    <p>
+                                        test test test test test test test test test 
+                                    </p>
+                                </motion.div>
+                            )}
+                        </motion.div>
                     </div>
                 </div>
             </div>
