13de4d327596e74b8c04f9fa61b3fa65b326f314
GustavoBoscoGit <gustavobosco1@gmail.com>
2023-11-14 21:10:47 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import{ createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
@@ -18,8 +17,6 @@ const router = createBrowserRouter([
     element: <WeekPredictionPage/>
   }
 ])
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
