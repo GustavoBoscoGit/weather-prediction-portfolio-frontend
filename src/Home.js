34af14d89720cb934aa0593e602e8ddce56e4833
GustavoBoscoGit <gustavobosco1@gmail.com>
2023-11-22 11:16:50 -0300

    armezar os dados da previsão

--- src/Home.js
+++ src/Home.js
@@ -32,64 +32,13 @@ function Home() {
 
   const formattedDate = currentDate.toLocaleDateString(undefined, options);
 
-  // const [city, setCity] = useState('');
-  
-  // const handleSearch = () => {
-  //   const APIKey = '52d00d043b05a31a967aaad360a28c91';
-    
-  //   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},BR&appid=${APIKey}`)
-  //     .then(response => response.json())
-  //     .then(json => {
-  //       const description = document.querySelector('.climaDia');
-  //       const dayTemperature = document.querySelector('.temperaturaDiaWidget');
-  //       const dayMaxTemperature = document.querySelector('.maxTemperature');
-  //       const dayMinTemperature = document.querySelector('.minTemperature');
-  //       const humidity = document.querySelector('.humidadePorcentagem');
-  //       const windSpeed = document.querySelector('.ventoValor');
-
-  //       dayTemperature.innerHTML = `${(parseInt(json.main.temp) - 273)}º`;
-  //       description.innerHTML = `${json.weather[0].description}`;
-  //       humidity.innerHTML = `${json.main.humidity}%`;
-  //       windSpeed.innerHTML = `${parseInt(json.wind.speed)}km/h`;
-  //       dayMaxTemperature.innerHTML = `${(parseInt(json.main.temp_max) - 273)}º`;
-  //       dayMinTemperature.innerHTML = `${(parseInt(json.main.temp_min) - 273)}º`;
-  //     });
-      
-  //     const cardDate = {
-  //       day: "2-digit",
-  //       month: "short",
-  //       timeZone: "UTC",
-  //       locale: "pt-BR",
-  //     };
-
-  //     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},BR&appid=${APIKey}`)
-  //     .then(response => response.json())
-  //     .then(json => {
-  //       const weekTemperature1 = document.querySelector('.temperaturaDiaCard1');
-  //       const weekTemperature2 = document.querySelector('.temperaturaDiaCard2');
-  //       const weekTemperature3 = document.querySelector('.temperaturaDiaCard3');
-  //       const weekTemperature4 = document.querySelector('.temperaturaDiaCard4');
-
-  //       weekTemperature1.innerHTML = `${(parseInt(json.list[5].main.temp) - 273)}º`;
-  //       weekTemperature2.innerHTML = `${(parseInt(json.list[10].main.temp) - 273)}º`;
-  //       weekTemperature3.innerHTML = `${(parseInt(json.list[20].main.temp) - 273)}º`;
-  //       weekTemperature4.innerHTML = `${(parseInt(json.list[30].main.temp) - 273)}º`;
-        
-  //       const weekDay1 = document.querySelector('.dataMesCard1');
-  //       const weekDay2 = document.querySelector('.dataMesCard2');
-  //       const weekDay3 = document.querySelector('.dataMesCard3');
-  //       const weekDay4 = document.querySelector('.dataMesCard4');
-
-  //       weekDay1.innerHTML = new Date(`${json.list[5].dt_txt}`).toLocaleDateString(undefined, cardDate).replace(" de", "").replace(".", "");
-  //       weekDay2.innerHTML = new Date(`${json.list[10].dt_txt}`).toLocaleDateString(undefined, cardDate).replace(" de", "").replace(".", "");
-  //       weekDay3.innerHTML = new Date(`${json.list[20].dt_txt}`).toLocaleDateString(undefined, cardDate).replace(" de", "").replace(".", "");
-  //       weekDay4.innerHTML = new Date(`${json.list[30].dt_txt}`).toLocaleDateString(undefined, cardDate).replace(" de", "").replace(".", "");
-  //     })
-  // };
-
   const [city, setCity] = useState('');
-  const [currentWeatherData, setCurrentWeatherData] = useState(null);
-  const [forecastWeatherData, setForecastWeatherData] = useState(null);
+  const [currentWeatherData, setCurrentWeatherData] = useState(
+    JSON.parse(localStorage.getItem('currentWeather')) || null
+  );
+  const [forecastWeatherData, setForecastWeatherData] = useState(
+    JSON.parse(localStorage.getItem('forecastWeather')) || null
+  );
 
   const handleSearch = () => {
     const APIKey = '52d00d043b05a31a967aaad360a28c91';
@@ -105,6 +54,15 @@ function Home() {
           maxTemperature: parseInt(json.main.temp_max) - 273,
           minTemperature: parseInt(json.main.temp_min) - 273,
         });
+
+        localStorage.setItem('currentWeather', JSON.stringify({
+          description: json.weather[0].description,
+          temperature: parseInt(json.main.temp) - 273,
+          humidity: json.main.humidity,
+          windSpeed: parseInt(json.wind.speed),
+          maxTemperature: parseInt(json.main.temp_max) - 273,
+          minTemperature: parseInt(json.main.temp_min) - 273,
+        }));
       });
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},BR&appid=${APIKey}`)
@@ -116,7 +74,7 @@ function Home() {
           timeZone: "UTC",
           locale: "pt-BR",
         };
-
+        
         const forecastData = [];
         [5, 10, 20, 30].forEach(index => {
           forecastData.push({
@@ -124,6 +82,8 @@ function Home() {
             date: new Date(`${json.list[index].dt_txt}`).toLocaleDateString(undefined, cardDate).replace(" de", "").replace(".", ""),
           });
         });
+
+        localStorage.setItem('forecastWeather', JSON.stringify(forecastData))
 
         setForecastWeatherData(forecastData);
       });
@@ -187,4 +147,3 @@ function Home() {
 
 export default Home;
 
-// os dados foram apresentados corretamente, porém ao mudar para a outra página os dados sumiram, em minha aplicação possuo as página "/" que é a página home e "/semana" que é uma página secundária. Quando clico para ir á página "/semana" e depois clico para voltar a página "/" os dados somem, como faço para corrigir isso?
